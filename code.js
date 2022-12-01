1f1a5ca973024f52fbbb8292a7265f60ef399ec7
Alan.o <olmedoalan821@gmail.com>
2022-12-01 13:53:33 -0300

    form-validate

--- code.js
+++ code.js
@@ -12,3 +12,24 @@ window.addEventListener("load", () =>{
 //     spinner.style.opacity = "0";
 
 // }, 1000)
+
+
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
